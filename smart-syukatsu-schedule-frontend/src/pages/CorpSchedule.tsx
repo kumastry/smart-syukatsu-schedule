f37eafb4a3a7d120fb5806ee8a4e780ff75c5450
kumastry <chma19063@g.nihon-u.ac.jp>
2023-09-19 00:08:48 +0900

    feature-画面レイアウトの作成 #2 スケジュール画面を追加した

--- smart-syukatsu-schedule-frontend/src/pages/CorpSchedule.tsx
+++ smart-syukatsu-schedule-frontend/src/pages/CorpSchedule.tsx
@@ -1,36 +1,63 @@
 import { useState } from 'react';
-import { Message, getMessage } from '../data/messages';
 import {
-  IonBackButton,
-  IonButtons,
-  IonContent,
-  IonHeader,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonNote,
-  IonPage,
-  IonToolbar,
-  useIonViewWillEnter,
+    IonContent,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonPage,
+    IonToolbar,
+    useIonViewWillEnter,
+    IonFooter,
+    IonBadge,
 } from '@ionic/react';
-import { personCircle } from 'ionicons/icons';
 import { useParams } from 'react-router';
-import './ViewMessage.css';
+
+import Header from '../components/Header/Header';
+import text from './../assets/text.json';
+import FabButton from '../components/FabButton/FabButton';
+import { sampleCorpSchedules } from './../data/sampleData';
+import { chevronDown } from 'ionicons/icons';
+
 
 function CorpSchedule() {
-    const [message, setMessage] = useState<Message>();
+    const [schedules, SetSchedules] = useState<any>([]);
     const params = useParams<{ id: string }>();
 
     useIonViewWillEnter(() => {
+        SetSchedules(
+            sampleCorpSchedules.filter((item: any, key:Number) => {
+                return item.corpId === params.id;
+            })
+        )
     });
 
     return (
-      <IonPage id="view-message-page">
-        <IonContent>
+        <IonPage id="corp-schedules-page">
+            <Header headerText={text.schedule} isShowBackButton={true} />
+            <IonContent fullscreen>
 
-        </IonContent>
-      </IonPage>
+                <h1 style={{margin:'1rem'}}>企業名（仮）</h1>
+                {/**IonAccordionを使う**/}
+                {
+                    schedules.map((item:any, key:Number) => {
+                        return (
+                            <IonItem >
+                                <IonLabel>{item.corpPeriod.slice(5)}</IonLabel>
+                                <IonLabel>17:00-18:00</IonLabel>
+                                <IonBadge color="success">{item.corpEvent}</IonBadge>
+                                <IonIcon aria-hidden="true" icon={chevronDown}/>
+                            </IonItem>
+                        );
+                    })
+                }
+            </IonContent>
+            <IonFooter>
+                <IonToolbar>
+                    <FabButton />
+                </IonToolbar>
+            </IonFooter>
+        </IonPage>
     );
-  }
+}
 
-  export default CorpSchedule;
+export default CorpSchedule;
