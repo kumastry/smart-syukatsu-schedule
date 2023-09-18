c0f8998218a436d1bafcbc1ae62c5ae4b4faeea0
kumastry <chma19063@g.nihon-u.ac.jp>
2023-09-19 00:05:48 +0900

    feature-画面レイアウトの作成 #2 サンプルスケジュールデータの追加

--- smart-syukatsu-schedule-frontend/src/data/sampleData.ts
+++ smart-syukatsu-schedule-frontend/src/data/sampleData.ts
@@ -1,5 +1,6 @@
 
 export interface HomePageData {
+    id:string;
     corpName: string;
     corpEvent: string;
     corpPeriod: string;
@@ -8,54 +9,63 @@ export interface HomePageData {
 
 export const sampleHomePageData:HomePageData[] = [
     {
+        id:'1',
         corpName:'株式会社サンプル１',
         corpEvent: '面談',
         corpPeriod:"2023年9月8日",
         corpNote:"・○○さんとの定期面談 <br>・質問事項を考える<br> ・普段着で着ていく <br>"
     },
     {
+        id:'2',
         corpName:'株式会社サンプル２',
         corpEvent: '説明会',
         corpPeriod:"2023年9月16日",
         corpNote:"・○○さんとの定期面談 <br>・質問事項を考える<br> ・普段着で着ていく <br>"
     },
     {
+        id:'3',
         corpName:'株式会社サンプル３',
         corpEvent: '適性試験',
         corpPeriod:"2023年10月8日",
         corpNote:"・○○さんとの定期面談 <br>・質問事項を考える<br> ・普段着で着ていく <br>"
     },
     {
+        id:'4',
         corpName:'株式会社サンプル４',
         corpEvent: 'コーディングテスト',
         corpPeriod:"2023年10月18日",
         corpNote:"・○○さんとの定期面談 <br>・質問事項を考える<br> ・普段着で着ていく <br>"
     },
     {
+        id:'5',
         corpName:'株式会社サンプル５',
         corpEvent: '面接',
         corpPeriod:"2023年11月8日",
         corpNote:"・○○さんとの定期面談 <br>・質問事項を考える<br> ・普段着で着ていく <br>"
     },
     {
+        id:'6',
         corpName:'株式会社サンプル6',
         corpEvent: '面接',
         corpPeriod:"2023年11月20日",
         corpNote:"・○○さんとの定期面談 <br>・質問事項を考える<br> ・普段着で着ていく <br>"
     },
     {
+        id:'7',
         corpName:'株式会社サンプル7',
         corpEvent: '面談',
         corpPeriod:"2023年12月3日",
         corpNote:"・○○さんとの定期面談 <br>・質問事項を考える<br> ・普段着で着ていく <br>"
     },
     {
+        id:'8',
         corpName:'株式会社サンプル8',
         corpEvent: '説明会',
         corpPeriod:"2023年12月3日",
         corpNote:"・○○さんとの定期面談 <br>・質問事項を考える<br> ・普段着で着ていく <br>"
     },
     {
+        id:'9',
         corpName:'株式会社サンプル9',
         corpEvent: '面談',
         corpPeriod:"2023年12月3日",
@@ -63,5 +73,39 @@ export const sampleHomePageData:HomePageData[] = [
     }
 ]
 
+export const sampleCorpSchedules: any = [
+    {
+        id:'1',
+        corpId:'1',
+        corpEvent: '面談',
+        corpPeriod:"2023年9月8日",
+        corpNote:"・○○さんとの定期面談 <br>・質問事項を考える<br> ・普段着で着ていく <br>"
+    },
 
-export const getSampleHomePageData = () => sampleHomePageData;
+    {
+        id:'2',
+        corpId:'1',
+        corpEvent: 'コーディングテスト',
+        corpPeriod:"2023年9月20日",
+        corpNote:"コーディングテスト締め切り"
+    },
+    {
+        id:'3',
+        corpId:'1',
+        corpEvent: 'ES',
+        corpPeriod:"2023年10月1日",
+        corpNote:"ES締め切り"
+    },
+    {
+        id:'3',
+        corpId:'1',
+        corpEvent: '適性試験',
+        corpPeriod:"2023年10月5日",
+        corpNote:"適性試験締め切り"
+    },
+]
+
+
+
+
+
