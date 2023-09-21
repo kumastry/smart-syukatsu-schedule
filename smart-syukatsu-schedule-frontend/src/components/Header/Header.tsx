d1592066fd4afa98c36763df4c1f97895a07f761
kumastry <chma19063@g.nihon-u.ac.jp>
2023-09-21 14:55:10 +0900

    feature-画面レイアウトの作成 #2 prettierによる整形

--- smart-syukatsu-schedule-frontend/src/components/Header/Header.tsx
+++ smart-syukatsu-schedule-frontend/src/components/Header/Header.tsx
@@ -1,43 +1,60 @@
-import React from 'react';
-import { IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonButton, IonSearchbar, IonIcon } from '@ionic/react';
-import text from './../../assets/text.json';
-import { menu } from 'ionicons/icons';
-
+import React from "react";
+import {
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonBackButton,
+  IonButtons,
+  IonButton,
+  IonSearchbar,
+  IonIcon,
+} from "@ionic/react";
+import text from "./../../assets/text.json";
+import { menu } from "ionicons/icons";
 
 type Props = {
-    headerText: string
-    isShowBackButton?: boolean
-    backButtonText?: string
-    backButtonHref?: string
-    isShowSearchBar?: boolean
-}
+  headerText: string;
+  isShowBackButton?: boolean;
+  backButtonText?: string;
+  backButtonHref?: string;
+  isShowSearchBar?: boolean;
+};
 
-const Header = ({ headerText, isShowBackButton = false, backButtonText = text.top, backButtonHref = "/home", isShowSearchBar = false }: Props) => {
-    return (
-        <>
-            <IonHeader>
-                <IonToolbar>
-                    {isShowBackButton &&
-                        <IonButtons slot="start">
-                            <IonBackButton text={backButtonText} defaultHref={backButtonHref}></IonBackButton>
-                        </IonButtons>
-                    }
-                    <IonTitle>{headerText}</IonTitle>
+const Header = ({
+  headerText,
+  isShowBackButton = false,
+  backButtonText = text.top,
+  backButtonHref = "/home",
+  isShowSearchBar = false,
+}: Props) => {
+  return (
+    <>
+      <IonHeader>
+        <IonToolbar>
+          {isShowBackButton && (
+            <IonButtons slot="start">
+              <IonBackButton
+                text={backButtonText}
+                defaultHref={backButtonHref}
+              ></IonBackButton>
+            </IonButtons>
+          )}
+          <IonTitle>{headerText}</IonTitle>
 
-                    <IonButtons slot="end">
-                        <IonButton>
-                            <IonIcon aria-hidden="true" icon={menu} />
-                        </IonButton>
-                    </IonButtons>
-                </IonToolbar>
-                {isShowSearchBar &&
-                    <IonToolbar>
-                        <IonSearchbar placeholder={text.searchByCorp}></IonSearchbar>
-                    </IonToolbar>
-                }
-            </IonHeader>
-        </>
-    );
-}
+          <IonButtons slot="end">
+            <IonButton>
+              <IonIcon aria-hidden="true" icon={menu} />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+        {isShowSearchBar && (
+          <IonToolbar>
+            <IonSearchbar placeholder={text.searchByCorp}></IonSearchbar>
+          </IonToolbar>
+        )}
+      </IonHeader>
+    </>
+  );
+};
 
 export default Header;
