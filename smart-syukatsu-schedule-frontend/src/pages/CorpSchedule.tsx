7f5cb0ea4e80e36b95afd3469becd79a97c11e1a
kumastry <chma19063@g.nihon-u.ac.jp>
2023-09-21 14:54:18 +0900

    feature-画面レイアウトの作成 #2 ESlintのエラーを削除

--- smart-syukatsu-schedule-frontend/src/pages/CorpSchedule.tsx
+++ smart-syukatsu-schedule-frontend/src/pages/CorpSchedule.tsx
@@ -1,63 +1,61 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
-    IonContent,
-    IonIcon,
-    IonItem,
-    IonLabel,
-    IonPage,
-    IonToolbar,
-    useIonViewWillEnter,
-    IonFooter,
-    IonBadge,
-} from '@ionic/react';
-import { useParams } from 'react-router';
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonToolbar,
+  useIonViewWillEnter,
+  IonFooter,
+  IonBadge,
+} from "@ionic/react";
+import { useParams } from "react-router";
 
-import Header from '../components/Header/Header';
-import text from './../assets/text.json';
-import FabButton from '../components/FabButton/FabButton';
-import { sampleCorpSchedules } from './../data/sampleData';
-import { chevronDown } from 'ionicons/icons';
+import Header from "../components/Header/Header";
+import text from "./../assets/text.json";
+import FabButton from "../components/FabButton/FabButton";
+import { sampleCorpSchedules } from "./../data/sampleData";
+import { chevronDown } from "ionicons/icons";
 
+import type { CorpScheduleData } from "./../data/sampleData";
 
 function CorpSchedule() {
-    const [schedules, SetSchedules] = useState<any>([]);
-    const params = useParams<{ id: string }>();
+  const [schedules, SetSchedules] = useState<CorpScheduleData[]>([]);
+  const params = useParams<{ id: string }>();
 
-    useIonViewWillEnter(() => {
-        SetSchedules(
-            sampleCorpSchedules.filter((item: any, key:Number) => {
-                return item.corpId === params.id;
-            })
-        )
-    });
-
-    return (
-        <IonPage id="corp-schedules-page">
-            <Header headerText={text.schedule} isShowBackButton={true} />
-            <IonContent fullscreen>
-
-                <h1 style={{margin:'1rem'}}>企業名（仮）</h1>
-                {/**IonAccordionを使う**/}
-                {
-                    schedules.map((item:any, key:Number) => {
-                        return (
-                            <IonItem >
-                                <IonLabel>{item.corpPeriod.slice(5)}</IonLabel>
-                                <IonLabel>17:00-18:00</IonLabel>
-                                <IonBadge color="success">{item.corpEvent}</IonBadge>
-                                <IonIcon aria-hidden="true" icon={chevronDown}/>
-                            </IonItem>
-                        );
-                    })
-                }
-            </IonContent>
-            <IonFooter>
-                <IonToolbar>
-                    <FabButton />
-                </IonToolbar>
-            </IonFooter>
-        </IonPage>
+  useIonViewWillEnter(() => {
+    SetSchedules(
+      sampleCorpSchedules.filter((item) => {
+        return item.corpId === params.id;
+      }),
     );
+  });
+
+  return (
+    <IonPage id="corp-schedules-page">
+      <Header headerText={text.schedule} isShowBackButton={true} />
+      <IonContent fullscreen>
+        <h1 style={{ margin: "1rem" }}>企業名（仮）</h1>
+        {/**IonAccordionを使う**/}
+        {schedules.map((item, key) => {
+          return (
+            <IonItem key={key}>
+              <IonLabel>{item.corpPeriod.slice(5)}</IonLabel>
+              <IonLabel>17:00-18:00</IonLabel>
+              <IonBadge color="success">{item.corpEvent}</IonBadge>
+              <IonIcon aria-hidden="true" icon={chevronDown} />
+            </IonItem>
+          );
+        })}
+      </IonContent>
+      <IonFooter>
+        <IonToolbar>
+          <FabButton />
+        </IonToolbar>
+      </IonFooter>
+    </IonPage>
+  );
 }
 
 export default CorpSchedule;
--- smart-syukatsu-schedule-frontend/src/pages/Home.tsx
+++ smart-syukatsu-schedule-frontend/src/pages/Home.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
 import {
   IonContent,
   IonList,
   IonPage,
-  IonFooter, IonToolbar
-} from '@ionic/react';
-import './Home.css';
+  IonFooter,
+  IonToolbar,
+} from "@ionic/react";
+import "./Home.css";
 
-import Header from '../components/Header/Header';
-import CorpListItem from '../components/CorpListItem/CorpListItem';
-import FabButton from '../components/FabButton/FabButton';
-import text from './../assets/text.json';
-import { sampleHomePageData } from './../data/sampleData';
+import Header from "../components/Header/Header";
+import CorpListItem from "../components/CorpListItem/CorpListItem";
+import FabButton from "../components/FabButton/FabButton";
+import text from "./../assets/text.json";
+import { sampleHomePageData } from "./../data/sampleData";
 
 const Home: React.FC = () => {
   return (
@@ -19,11 +19,18 @@ const Home: React.FC = () => {
       <Header headerText={text.appTitle} isShowSearchBar={true} />
       <IonContent fullscreen>
         <IonList>
-          {
-            sampleHomePageData.map((item) => {
-              return <CorpListItem id = {item.id} corpName={item.corpName} corpEvent={item.corpEvent} corpPeriod={item.corpPeriod} corpNote={item.corpNote} />;
-            })
-          }
+          {sampleHomePageData.map((item, key) => {
+            return (
+              <CorpListItem
+                key={key}
+                id={item.id}
+                corpName={item.corpName}
+                corpEvent={item.corpEvent}
+                corpPeriod={item.corpPeriod}
+                corpNote={item.corpNote}
+              />
+            );
+          })}
         </IonList>
       </IonContent>
 
