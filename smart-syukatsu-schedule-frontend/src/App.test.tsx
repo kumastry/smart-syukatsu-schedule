3fc0ef83ba5a5d51c6c7f4b3e95038f8d1468c2c
kumastry <chma19063@g.nihon-u.ac.jp>
2023-09-23 06:08:53 +0900

    other-actions_devのテスト

--- smart-syukatsu-schedule-frontend/src/App.test.tsx
+++ smart-syukatsu-schedule-frontend/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-// app test234
+// app test2345
 test("renders without crashing", () => {
   const { baseElement } = render(<App />);
   expect(baseElement).toBeDefined();
