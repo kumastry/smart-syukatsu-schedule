66672b4f9747f55af2e6b9a527ac63c08b5ca92f
kumastry <chma19063@g.nihon-u.ac.jp>
2023-10-04 01:57:08 +0900

    feature-backendの作成 /schdulesを一通り完成

--- smart-syukatsu-schedule-backend/src/routes/schedule.ts
+++ smart-syukatsu-schedule-backend/src/routes/schedule.ts
@@ -1,25 +1,108 @@
 import express, { Request, Response } from "express";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import { corporations, schedules } from "./../../db/schema";
+import { notifications, schedules } from "./../../db/schema";
 import { eq } from "drizzle-orm";
-import { oneOf, query } from "express-validator";
+import db from "./../../db/db";
 
 const router = express.Router();
-const connectionString = Bun.env.DATABASE_URL as string;
-const client = postgres(connectionString);
-const db = drizzle(client);
+
+type scheduleParamT = {
+  scheduleId: number;
+};
+
+type QueryT = {
+    limit? : number;
+    offset? : number;
+}
+
+type notificationBodyT = {
+  notificationTime: Date;
+};
 
 const schedulesRoute = () => {
-  router.get("/schedules/:scheduleId", async (req, res) => {});
+  router.get("/", async (req: Request<{}, {}, {}, QueryT>, res: Response) => {
+    const limit = req.query.limit || 50;
+    const offset = req.query.offset || 0;
+    if(isNaN(limit) || isNaN(offset)) return res.sendStatus(403);
+    const schs = await db.select().from(schedules).limit(limit).offset(offset);
+    if(!schs.length) return res.sendStatus(404);
+    return res.status(200).json(schs);
+  });
 
-  router.patch("/schedules/:scheduleId", async (req, res) => {});
+  router.get(
+    "/:scheduleId",
+    async (req: Request<scheduleParamT>, res: Response) => {
+      const scheduleId = req.params.scheduleId;
+      if (isNaN(scheduleId)) res.sendStatus(403);
 
-  router.delete("/schedules/:scheduleId", async (req, res) => {});
+      const sch = await db
+        .select()
+        .from(schedules)
+        .where(eq(schedules.id, scheduleId));
 
-  router.get("/schedules/:scheduleId/notifications", async (req, res) => {});
+      if (!sch.length) res.sendStatus(404);
+      return res.status(200).json(sch[0]);
+    },
+  );
 
-  router.post("/schedules/:scheduleId/notifications", async (req, res) => {});
+  router.patch(
+    "/:scheduleId",
+    async (req: Request<scheduleParamT>, res: Response) => {
+      // do after
+    },
+  );
+
+  router.delete(
+    "/:scheduleId",
+    async (req: Request<scheduleParamT>, res: Response) => {
+      const scheduleId = req.params.scheduleId;
+      if (isNaN(scheduleId)) res.sendStatus(403);
+
+      const deletedSch = await db
+        .delete(schedules)
+        .where(eq(schedules.id, scheduleId))
+        .returning();
+
+      if (!deletedSch.length) res.sendStatus(404);
+      return res.status(200).json(deletedSch[0]);
+    },
+  );
+
+  router.get(
+    "/:scheduleId/notifications",
+    async (req: Request<scheduleParamT>, res: Response) => {
+      const scheduleId = req.params.scheduleId;
+      if (isNaN(scheduleId)) return res.sendStatus(403);
+
+      const notis = await db
+        .select()
+        .from(notifications)
+        .where(eq(notifications.scheduleId, scheduleId));
+
+      if (!notis.length) return res.sendStatus(404);
+
+      return res.status(200).json(notis);
+    },
+  );
+
+  router.post(
+    "/:scheduleId/notifications",
+    async (
+      req: Request<scheduleParamT, {}, notificationBodyT>,
+      res: Response,
+    ) => {
+      const scheduleId = req.params.scheduleId;
+      const notificationTime = new Date(req.body.notificationTime);
+      if (isNaN(scheduleId)) return res.sendStatus(403);
+
+      const insertedNotis = await db
+        .insert(notifications)
+        .values({ notificationTime: notificationTime, scheduleId: scheduleId })
+        .returning();
+
+      return res.status(200).json(insertedNotis);
+    },
+  );
+
   return router;
 };
 
