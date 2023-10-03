ef369d215299ac198cb7d84517e0c52f40e00dfc
kumastry <chma19063@g.nihon-u.ac.jp>
2023-10-04 01:54:34 +0900

    feature-backendの作成 マイグレーションのリネーム

---
+++ smart-syukatsu-schedule-backend/db/dbMigration.ts
@@ -0,0 +1,8 @@
+import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
+
+const connectionString = Bun.env.DATABASE_URL;
+const migrationClient = postgres(connectionString, { max: 1 });
+console.log("await migration....");
+await migrate(drizzle(migrationClient), { migrationsFolder: "./drizzle" });
