1e91e407c15cfde3a8146777010e678b8bec344f
kumastry <chma19063@g.nihon-u.ac.jp>
2023-10-04 04:17:00 +0900

    feature-backendの作成 typeエラーの修正

--- smart-syukatsu-schedule-backend/db/dbMigration.ts
+++ smart-syukatsu-schedule-backend/db/dbMigration.ts
@@ -1,8 +1,12 @@
-import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-const connectionString = Bun.env.DATABASE_URL;
+const connectionString = Bun.env.DATABASE_URL as string;
 const migrationClient = postgres(connectionString, { max: 1 });
 console.log("await migration....");
-await migrate(drizzle(migrationClient), { migrationsFolder: "./drizzle" });
+( async() =>
+    {
+        await migrate(drizzle(migrationClient), { migrationsFolder: "./../drizzle" })
+    }
+)();
--- smart-syukatsu-schedule-backend/drizzle.config.ts
+++ smart-syukatsu-schedule-backend/drizzle.config.ts
@@ -5,6 +5,6 @@ export default {
   out: "./drizzle",
   driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: Bun.env.DATABASE_URL as string,
   }
 } satisfies Config;
